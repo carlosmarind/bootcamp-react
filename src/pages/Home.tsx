2dc96ca030eb9169dffc87e37e5c2826f45014f2
Carlos Marin Diaz <carlosmarind@gmail.com>
2025-09-30 19:51:33 -0300

    actualizacion con uso de estructuras json

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,26 @@
-import cliente from '../data/cliente.json'
+import { type Product } from "../types/product";
+import productos from "../data/productos.json"
 
 function Home() {
 
+    const listaProductos: Product[] = productos as Product[];
 
     return (
         <>
             <h2>Pagina de Home</h2>
-
-            <p>Bienvenida {cliente.nombre} {cliente.apellido}, tu estado es {cliente.activo}</p>
+            <ul>
+                {
+                    listaProductos.map((producto) => {
+                        //esta funcion del map se va a ejecutar por cada "producto"
+                        return (
+                            <>
+                                <li key={producto.id}> {producto.nombre} - {producto.stock} -  {producto.uuid}</li>
+                            </>
+                        )
+                    })
+                }
+            </ul>
         </>
-
     )
 }
 export { Home }
---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+type Product = {
+    id: number;
+    uuid: string;
+    nombre: string;
+    valor: number;
+    stock?: number;
+    sku?: string;
+    fechaCreacion?: Date;
+}
+
+export type { Product };
