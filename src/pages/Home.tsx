32a1dc43923e572343b010ac59f1e5e671c165ae
Carlos Marin Diaz <carlosmarind@gmail.com>
2025-09-30 18:25:43 -0300

    import desde json

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import cliente from '../data/cliente.json'
+
 function Home() {
+
+
     return (
         <>
             <h2>Pagina de Home</h2>
+
+            <p>Bienvenida {cliente.nombre} {cliente.apellido}, tu estado es {cliente.activo}</p>
         </>
 
     )
