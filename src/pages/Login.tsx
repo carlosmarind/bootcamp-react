91a43ad24c271eb86a08bd8352e1710da73a2e8b
Carlos Marin Diaz <carlosmarind@gmail.com>
2025-11-25 20:31:42 -0300

    actualizacion con login a backend

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,46 @@
+import React, { useState } from "react"
+
 function Login() {
+
+    const [form, setForm] = useState({ user: '', password: '' });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        fetch("http://localhost:3001/login", {
+            method: "POST",
+            body: JSON.stringify({ username: form.user, password: form.password }),
+            headers: {
+                "Content-Type": 'application/json'
+            }
+            /*{
+                "username": form.user
+                "password": form.password
+            }*/
+        })
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     Usuario:
-                    <input name="user" type="text" />
+                    <input name="user" type="text" value={form.user} onChange={handleChange} />
                 </label>
                 <label>
                     Contraseña:
-                    <input name="password" type="password" />
+                    <input name="password" type="password" value={form.password} onChange={handleChange} />
                 </label>
-
+                <button type="submit">Enviar</button>
             </form>
         </div>
     )
 }
-
 export { Login }
